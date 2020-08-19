e0179b8106d7c13cb8ffbe279bf8253774cc68b8
andrecouto <coutoandreluis@gmail.com>
2020-08-19 09:47:58 -0300

    added ribbon and adjusts

--- src/main/kotlin/br/com/mastertech/couto/port/PortApplication.kt
+++ src/main/kotlin/br/com/mastertech/couto/port/PortApplication.kt
@@ -1,13 +1,16 @@
 package br.com.mastertech.couto.port
 
+import br.com.mastertech.couto.port.configuration.RibbonConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.ribbon.RibbonClients
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@RibbonClients(defaultConfiguration = [RibbonConfiguration::class])
 class PortApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/br/com/mastertech/couto/port/configuration/RibbonConfiguration.kt
+++ src/main/kotlin/br/com/mastertech/couto/port/configuration/RibbonConfiguration.kt
@@ -1,4 +1,13 @@
 package br.com.mastertech.couto.port.configuration
 
+import com.netflix.loadbalancer.IRule
+import com.netflix.loadbalancer.RandomRule
+import org.springframework.context.annotation.Bean
+
 class RibbonConfiguration {
+
+    @Bean
+    fun getRule(): IRule? {
+        return RandomRule()
+    }
 }
